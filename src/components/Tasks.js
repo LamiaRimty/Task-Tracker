a2530e4ab40f6dda413aebb57ef20932c399ab6f
Lamia Rimty <rimtycse@gmail.com>
2021-10-07 11:45:55 +0600

    Toggle Reminder

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,11 @@
 import Task from "./Task"
 
-const Tasks = ({ tasks,onDelete }) => {
+const Tasks = ({ tasks,onDelete,onToggle }) => {
     
     return (
         <>
             {tasks.map( (task)=>  //looping through them here, output of the component passing task={task} as a props
-             (<Task key={task.id} task={task} onDelete={onDelete} /> //when save that output my task 3 bar
+             (<Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} /> //when save that output my task 3 bar
             ))}
         </>
     )
